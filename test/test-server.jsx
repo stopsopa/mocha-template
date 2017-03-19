80a24c0b7c46f1132fe0480574b48bf59440353d
simon <grytysek@gmail.com>
2017-03-19 18:38:53 +0100

    For ports greater than 1024, using sudo is not necessary

--- test/test-server.jsx
+++ test/test-server.jsx
@@ -4,22 +4,22 @@ const assert        = require('assert');
 const path          = require('path');
 const curl          = require(path.resolve(__dirname, '..', 'lib', 'curl.jsx'));
 
-// http://localhost:98/?url=http://localhost:99/
+// http://localhost:1026/?url=http://localhost:1025
 
 describe('Server and parser test', function () {
 
     it('server test', function (done) {
-        curl('http://localhost:99')
+        curl('http://localhost:1025')
             .then(function (res) {
-                assert.equal('Server: 0.0.0.0:99 is working...', res.body)
+                assert.equal('Server: 0.0.0.0:1025 is working...', res.body)
                 done()
             });
     })
 
     it('parser test', function (done) {
-        curl('http://localhost:98/?url=http://localhost:99')
+        curl('http://localhost:1026/?url=http://localhost:1026')
             .then(function (res) {
-                var expected = `<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">Server: 0.0.0.0:99 is working...</pre></body></html>`;
+                var expected = `<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">Server: 0.0.0.0:1026 is working...</pre></body></html>`;
                 assert.equal(expected, res.body)
                 done()
             });
